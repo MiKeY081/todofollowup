bae5692e51dcb9d77ca0764707b6b22ec69897c0
Mikie <kcmadhu081@gmail.com>
2023-12-23 17:43:07 +0545

    hello

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import axios from 'axios'
-axios.defaults.baseURL= "https://todofollowup-backend.vercel.app/api/v1"
+axios.defaults.baseURL= "https://todofollowup-backend.vercel.app/api/v1/todo"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
